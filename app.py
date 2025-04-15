3e863d71298671f52cee45eaed9a51d40ab24004
SurenderDTU <meenaroopwati005@gmail.com>
2025-04-15 05:56:16 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+model = joblib.load("model.pkl")
+
+@app.route('/')
+def home():
+    return "Iris Classifier is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    prediction = model.predict([data['features']])
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
